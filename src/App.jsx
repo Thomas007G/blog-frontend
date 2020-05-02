0a669239e76f8b0b8f6d72b8eee8bbe3a9a159c5
Thomas Daniel galligan <thomas007g@gmail.com>
2020-05-02 21:19:42 +0000

    fixed some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Nav, NavItem } from "./components/nav/Nav";
 
-import { ReactComponent as UserSVG } from "./icons/user.svg";
-import { ReactComponent as CogSVG } from "./icons/cog.svg";
-import { ReactComponent as ImageSVG } from "./icons/image.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
 // import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <Nav>
-        <NavItem icon={<UserSVG />} />
-        <NavItem icon={<CogSVG />} />
-        <NavItem icon={<ImageSVG />} />
+        <NavItem icon={<CogIcon />} />
       </Nav>
     );
   }
